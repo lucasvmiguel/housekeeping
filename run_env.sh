95a14a38b2a781919603e6a814272ef10360a4e3
Lucas Vieira <lucasvieira.dev@gmail.com>
2017-01-14 19:32:00 -0200

    docker compose -d

--- run_env.sh
+++ run_env.sh
@@ -2,5 +2,4 @@ git pull origin master
 sudo rm -rf tmp/pids/server.pid
 sudo docker-compose run web rake db:migrate RAILS_ENV="production"
 sudo docker-compose build
-sudo docker-compose up
-# docker-compose scale web=2
+sudo docker-compose up -d
